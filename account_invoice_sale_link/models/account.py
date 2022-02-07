f5fb500a7b9312afdcd84fe7395c48e518f49d40
Nick Booker <nick.booker@opusvl.com>
2022-02-07 13:32:59 +0000

    Import from odoo instead of from openerp

--- account_invoice_sale_link/models/account.py
+++ account_invoice_sale_link/models/account.py
@@ -20,7 +20,7 @@
 #
 ##############################################################################
 
-from openerp import models, fields, api
+from odoo import models, fields
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
